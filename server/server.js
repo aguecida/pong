9c618e5db41fc773d20dbb69aeab13747d5b6a2c
Daniele Agueci <d.agueci1@gmail.com>
2018-06-15 05:58:25 -0400

    Create new folder for classes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const socketIO = require('socket.io');
 
-const { Game } = require('./game');
+const { Game } = require('./models/game');
 
 const publicDir = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
